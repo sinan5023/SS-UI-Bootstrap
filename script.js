64f0a2b4eef25e4b60ef0b432ac7e9c0f6e519e5
Mohammed Sinan <sinan@mohammeds-MacBook-Air.local>
2025-08-12 15:22:34 +0530

    new-chnages

---
+++ script.js
@@ -0,0 +1,30 @@
+document.getElementById("Changeimage1").addEventListener("click",function(){
+    document.getElementById("Myimage").src="https://images.unsplash.com/photo-1472289065668-ce650ac443d2?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w5MTMyMXwwfDF8cmFuZG9tfHx8fHx8fHx8MTc0NzkwMDI4NXw&ixlib=rb-4.1.0&q=80&w=1080";
+
+})
+
+document.getElementById("Changeimage2").addEventListener("click",function(){
+    document.getElementById("Myimage").src="https://images.unsplash.com/photo-1653974123177-fe9c05fb79e6?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w5MTMyMXwwfDF8cmFuZG9tfHx8fHx8fHx8MTc0NzkwMDI4Nnw&ixlib=rb-4.1.0&q=80&w=1080";
+    
+})
+
+document.getElementById("Changeimage3").addEventListener("click",function(){
+    document.getElementById("Myimage").src="https://images.unsplash.com/photo-1529070538774-1843cb3265df?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w5MTMyMXwwfDF8cmFuZG9tfHx8fHx8fHx8MTc0NzkwMDI4NHw&ixlib=rb-4.1.0&q=80&w=1080";
+    
+})
+
+
+document.getElementById("Changeimage11").addEventListener("click",function(){
+    document.getElementById("Myimage1").src="https://images.unsplash.com/photo-1472289065668-ce650ac443d2?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w5MTMyMXwwfDF8cmFuZG9tfHx8fHx8fHx8MTc0NzkwMDI4NXw&ixlib=rb-4.1.0&q=80&w=1080";
+
+})
+
+document.getElementById("Changeimage22").addEventListener("click",function(){
+    document.getElementById("Myimage1").src="https://images.unsplash.com/photo-1653974123177-fe9c05fb79e6?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w5MTMyMXwwfDF8cmFuZG9tfHx8fHx8fHx8MTc0NzkwMDI4Nnw&ixlib=rb-4.1.0&q=80&w=1080";
+    
+})
+
+document.getElementById("Changeimage33").addEventListener("click",function(){
+    document.getElementById("Myimage1").src="https://images.unsplash.com/photo-1529070538774-1843cb3265df?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w5MTMyMXwwfDF8cmFuZG9tfHx8fHx8fHx8MTc0NzkwMDI4NHw&ixlib=rb-4.1.0&q=80&w=1080";
+    
+})
